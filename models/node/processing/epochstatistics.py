f8b0770e5ba5f7b3c8f00a3c59974695757bf769
Arthur <46134064+arthurhauer@users.noreply.github.com>
2023-07-11 14:19:26 -0300

    Mode -> EpochStatistics

--- models/node/processing/epochstatistics.py
+++ models/node/processing/epochstatistics.py
@@ -1,23 +1,40 @@
-from statistics import mode
+import statistics
 from typing import List, Dict, Final
 
 import numpy as np
 
+from models.exception.invalid_parameter_value import InvalidParameterValue
+from models.exception.missing_parameter import MissingParameterError
 from models.framework_data import FrameworkData
 from models.node.processing.processing_node import ProcessingNode
 
 
-class Mode(ProcessingNode):
-    _MODULE_NAME: Final[str] = 'node.processing.mode'
+class EpochStatistics(ProcessingNode):
+    _MODULE_NAME: Final[str] = 'node.processing.epochstatistics'
 
+    _ALLOWED_METHODS: Final[List[str]] = ['fmean', 'geometric_mean', 'harmonic_mean', 'mean', 'median',
+                                          'median_grouped', 'median_high', 'median_low', 'mode',
+                                          'pstdev', 'pvariance', 'stdev', 'variance']
     INPUT_MAIN: Final[str] = 'main'
     OUTPUT_MAIN: Final[str] = 'main'
 
     def _validate_parameters(self, parameters: dict):
         super()._validate_parameters(parameters)
+        if 'statistic' not in parameters:
+            raise MissingParameterError(module=self._MODULE_NAME, name=self.name,
+                                        parameter='statistic')
+        if type(parameters['statistic']) is not str:
+            raise InvalidParameterValue(module=self._MODULE_NAME, name=self.name,
+                                        parameter='statistic',
+                                        cause='must_be_string')
+        if parameters['statistic'] not in self._ALLOWED_METHODS:
+            raise InvalidParameterValue(module=self._MODULE_NAME, name=self.name,
+                                        parameter='statistic',
+                                        cause=f'must_be_one_of_{self._ALLOWED_METHODS}')
 
     def _initialize_parameter_fields(self, parameters: dict):
         super()._initialize_parameter_fields(parameters)
+        self._statistic_func = getattr(statistics, parameters['statistic'])
 
     def _is_next_node_call_enabled(self) -> bool:
         return self._output_buffer[self.OUTPUT_MAIN].get_data_count() > 0
@@ -32,7 +49,7 @@ class Mode(ProcessingNode):
         for channel in input_data.channels:
             formatted_data = []
             for epoch in input_data.get_data_on_channel(channel):
-                formatted_data.append(mode(epoch))
+                formatted_data.append(self._statistic_func(epoch))
             return_data.input_data_on_channel(np.asarray(formatted_data), channel)
         return {
             self.OUTPUT_MAIN: return_data
